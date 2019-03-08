74fdbaa4e75c9d199fd8f1fcae680e313df666ce
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-08 12:59:05 +0000

    Adicionei soluçao exercicio 8

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ValoresEspeciais
 {
@@ -6,8 +7,52 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            int a = Int32.MinValue;
+            int b = Int32.MaxValue;
+            double c = double.MinValue;
+            double d = double.MaxValue;
+            float e = float.MinValue;
+            float f = float.MaxValue;
+            short g = short.MinValue;
+            short h = short.MaxValue;
+            byte i = byte.MinValue;
+            byte j = byte.MaxValue;
+            long k = long.MinValue;
+            long l = long.MaxValue;
+            uint m = uint.MinValue;
+            uint n = uint.MaxValue;
+            ulong o = ulong.MinValue;
+            ulong p = ulong.MaxValue;
+            double q = double.NegativeInfinity;
+            double r = double.PositiveInfinity;
+            float s = float.NegativeInfinity;
+            float t = float.PositiveInfinity;
+            float u = float.NaN;
+
+            Console.WriteLine("Minimo de int "+ a);
+            Console.WriteLine("Máximo de int " + b);
+            Console.WriteLine("Minimo de double " + c);
+            Console.WriteLine("Máximo de double " + d);
+            Console.WriteLine("Minimo de float " + e);
+            Console.WriteLine("Máximo de float " + f);
+            Console.WriteLine("Minimo de short " + g);
+            Console.WriteLine("Máximo de short " + h);
+            Console.WriteLine("Minimo de byte " + i);
+            Console.WriteLine("Máximo de byte " + j);
+            Console.WriteLine("Minimo de long " + k);
+            Console.WriteLine("Máximo de long " + l);
+            Console.WriteLine("Minimo de uint " + m);
+            Console.WriteLine("Máximo de uint " + n);
+            Console.WriteLine("Minimo de ulong " + o);
+            Console.WriteLine("Máximo de ulong " + p);
+            Console.WriteLine("Infinito negativo de double " + q);
+            Console.WriteLine("Infinito positivo de double " + r);
+            Console.WriteLine("Infinito negativo de float " + s);
+            Console.WriteLine("Infinito positivo de float " + t);
+            Console.WriteLine("Não é númmero -> " + u);
             Console.WriteLine();
-            Console.WriteLine("Prime ENTER sair");
+            Console.WriteLine("Prime ENTER para sair");
             Console.ReadKey();
         }
     }
