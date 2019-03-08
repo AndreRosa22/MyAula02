44ee081d0d9f540732a7f1e3d5154b116e8ba89c
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-08 12:20:14 +0000

    adicionei solução exercicio 6

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Cilindro
 {
@@ -6,7 +7,22 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Insira uma altura");
+            string a = Console.ReadLine();//altura
+            int A = Convert.ToInt32(a);
+            Console.WriteLine("Insira um raio");
+            string r = Console.ReadLine();//raio
+            int R = Convert.ToInt32(r);
+            double volume = Math.PI * R * R *A ;//calcula o volume
+            double area = 2*Math.PI*R*(R + A);//calcula a area
+            string resultadoVolume = $"Volume do cilindro = \u03C0 * raio^2 * altura = {volume:f1}";
+            string resultadoArea = $"Area da superficie do cilindro = 2\u03C0 * raio + (raio + altura) = {area:f2}";
+            Console.WriteLine(resultadoVolume);
+            Console.WriteLine(resultadoArea);
+            Console.WriteLine();
+            Console.WriteLine("Prime ENTER para sair");
+            Console.ReadKey();
         }
     }
 }
