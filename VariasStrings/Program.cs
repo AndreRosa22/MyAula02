b7598526ae2d4109d7af9fc761e2e8a079891a30
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-08 09:28:27 +0000

    Adicionei varias strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,8 +6,20 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-            
+            string a = "Sou uma String Normal";
+            string b = @"String Verbatin(literal): ""C:\Users\Andre\Desktop""";
+            string c = "Unicode - \u00D8";
+            string d = "Outra string normal \t com um tab e uma barra \\";
+            string e = @"Outra string verbatin \u001B";
 
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine();
+            Console.WriteLine("Prime ENTER para continuar");
+            Console.ReadKey();
         }
     }
 }
